774b2aebe2ec2f9d18c8f0091ed8a5cd741b4ef5
vas-git-00 <vasyukov.a.o@gmail.com>
2024-10-17 14:14:29 +0300

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import PageNotFound from './pages/pageNotFound/PageNotFound'
 import UserSingle from './pages/users/userSingle/UserSingle'
 import Chats from './pages/chats/Chats'
 
+// test for commit
 
 function App() {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
